39aaad2f4b4e959b58ee4e9b8ad0ece3c2dab1d4
unknown <daniellourencoedu@gmail.com>
2023-11-20 15:55:02 +0100

    port issues

--- server/db.js
+++ server/db.js
@@ -5,7 +5,7 @@ const pool = new Pool({
 	user: process.env.DB_USERNAME,
 	password: process.env.DB_PASSWORD,
 	host: process.env.DB_HOST,
-	port: process.env.BD_PORT,
+	port: process.env.DB_PORT,
 	database: 'todoapp',
 });
 
--- server/server.js
+++ server/server.js
@@ -1,4 +1,4 @@
 const app = require('./app');
-const PORT = process.env.DB_PORT || 8000;
+const PORT = process.env.DB_PORT;
 
 app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
