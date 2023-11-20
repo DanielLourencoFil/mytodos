7ac29c6043d9d47da24b510a0ced571701d3dbe0
unknown <daniellourencoedu@gmail.com>
2023-11-20 17:05:13 +0100

    db  issues

--- server/db.js
+++ server/db.js
@@ -2,12 +2,17 @@ const Pool = require('pg').Pool;
 require('dotenv').config();
 
 const pool = new Pool({
-	user: process.env.DB_USERNAME,
-	password: process.env.DB_PASSWORD,
-	host: process.env.DB_HOST,
-	port: process.env.DB_PORT,
-	database: 'todoapp',
+	connectionString: process.env.DB_USERNAME,
 });
 
 console.log(process.env.DB_PASSWORD);
+// const pool = new Pool({
+// 	user: process.env.DB_USERNAME,
+// 	password: process.env.DB_PASSWORD,
+// 	host: process.env.DB_HOST,
+// 	port: process.env.DB_PORT,
+// 	database: 'todoapp',
+// });
+
+// console.log(process.env.DB_PASSWORD);
 module.exports = pool;
